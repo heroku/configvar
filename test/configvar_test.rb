968e37e10e49ae9d1afaca07430b7626e7993c53
Jamu Kakar <jkakar@kakar.ca>
2015-02-14 10:45:55 -0800

    Improve test.

--- test/configvar_test.rb
+++ test/configvar_test.rb
@@ -12,19 +12,19 @@ class ConfigVarTest < Minitest::Test
     set_env('PORT', '8080')
     set_env('ENABLED', '1')
     config = ConfigVar.define do
-      required_string :database_url
-      required_int    :port
-      required_bool   :enabled
-      optional_string :name,    'Bob'
-      optional_int    :age,     42
-      optional_bool   :balding, true
+      required_string  :database_url
+      required_int     :port
+      required_bool    :enabled
+      optional_string  :name,      'Bob'
+      optional_int     :age,       42
+      optional_bool    :friendly,  true
     end
     assert_equal('postgres:///example', config[:database_url])
     assert_equal(8080, config[:port])
     assert_equal(true, config[:enabled])
     assert_equal('Bob', config[:name])
     assert_equal(42, config[:age])
-    assert_equal(true, config[:balding])
+    assert_equal(true, config[:friendly])
   end
 end
 
