4c8ec6ae5304bb05ccb15c660f08cf25f8948ed4
Jamu Kakar <jkakar@kakar.ca>
2015-02-16 13:46:10 -0800

    Fix docstring error.

--- lib/configvar.rb
+++ lib/configvar.rb
@@ -2,10 +2,10 @@ module ConfigVar
   # Define required and optional configuration variables and load them from
   # the environment.  Returns a configuration object that can be treated like
   # a Hash with values available using lowercase symbols.  For example, a PORT
-  # value from the environment can be accesses as config[:port] in the
-  # returned object.  Booleans are only considered valid if they are one of
-  # '0', '1', 'true' or 'false'.  The values are case insensitive, so 'TRUE',
-  # 'True' and 'TrUe' are all valid booleans.
+  # value from the environment can be accesses as config.port in the returned
+  # object.  Booleans are only considered valid if they are one of '0', '1',
+  # 'true' or 'false'.  The values are case insensitive, so 'TRUE', 'True' and
+  # 'TrUe' are all valid booleans.
   #
   # Example:
   #
